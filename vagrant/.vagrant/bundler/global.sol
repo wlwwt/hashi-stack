e4ce8356e824da5d8cdc01150a46a0ab3db39de9
Wolfgang Freund <wf@communiacs.de>
2022-03-13 20:33:55 +0100

    [TASK] switched to latest ubuntu

--- vagrant/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-hostmanager",["= 1.8.9"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"48d8dcefc857f1b3aebceceb1e92f0ce881e283cb5d98edbef30caee44bdd567","vagrant_version":"2.2.19"}
